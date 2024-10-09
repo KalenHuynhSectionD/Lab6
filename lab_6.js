8312d692ad32dd4f0dacb36ca7efc0c1570d8d6b
KalenHuynhSectionD <n01694915@humber.ca>
2024-10-09 16:55:08 -0400

    lab6

---
+++ lab_6.js
@@ -0,0 +1,25 @@
+let myNumber = 10;
+let myString = "Hello Javascript!"
+
+console.log(myNumber);
+console.log(myString);
+
+for (let i = 0; i <= 10; i++){
+    console.log(i)
+}
+
+for (let i = 1; i <= 5; i++){
+    console.log(3 * 1);
+}
+
+function addNumbers(num1, num2){
+    return num1 + num2;
+}
+
+console.log(addNumbers(5, 10));
+
+function multiplyNumbers (num1, num2){
+    return num1 * num2
+}
+
+console.log(multiplyNumbers(8, 3));
